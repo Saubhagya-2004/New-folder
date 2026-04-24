6041c86e71c5cadeb03a84fe7d12b078c292f802
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-04-25 01:08:57 +0530

    develop

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,15 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL || "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
